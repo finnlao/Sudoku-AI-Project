930b99fea6603ef7e8ebda6bbe813103d46cef09
youssefe <elhasbani.youssef@gmail.com>
2018-11-26 17:38:18 -0500

    completed neighborhood search

--- algorithms/ArtificialBeeColony.java
+++ algorithms/ArtificialBeeColony.java
@@ -1,6 +1,7 @@
 package algorithms;
 
 import sudoku.Sudoku;
+import java.util.Random;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -14,8 +15,6 @@ public class ArtificialBeeColony{
 
     private Sudoku initialSudoku;
     public ArrayList<Sudoku> foodSources;
-
-    public float totalNector;
 
     /**
      * Constructs an Instance of the ArtificialBeeColony Sudoku Solver
@@ -44,11 +43,97 @@ public class ArtificialBeeColony{
             foodSource.generateBoard();
             foodSource.calculateFitness();
             foodSources.add(foodSource);
-
-            totalNector += foodSource.getFitnessLevel();
         }
         
         Collections.sort(foodSources);
+    }
+
+    /**
+     * Performs a Neighborhood Search on food souces
+     * Number of Search Steps is dependent on the number onlooker bees
+     * Food sources with higher nectar are allocated more bees
+     */
+    public void NeighborhoodSearch(){
+        Random random = new Random();
+        float totalNector = this.getTotalNector();
+
+
+        for(int i = 0; i < foodSources.size(); i++){
+            int searchSteps = Math.round(this.oBee * (foodSources.get(i).getFitnessLevel() / totalNector) + 1);
+
+            for(int s = 0; s < searchSteps; s++){
+                int k, jGrid, jCell;
+
+                // Pick a random food sources index
+                k = (int)(Math.random() * (foodSources.size() - 1));
+                
+                // Pick a random grid in the puzzle
+                jGrid = (int)(Math.random() * (foodSources.get(i).getBoard().size() - 1));
+
+                // Pick a random cell within the picked grid that isn't a fixed
+                do {
+                    jCell = (int)(Math.random() * 8);
+                } while (foodSources.get(i).getGrid(jGrid)[jCell].isFixed());
+                
+                int Xi = foodSources.get(i).getGrid(jGrid)[jCell].getValue();
+                int Xk = foodSources.get(k).getGrid(jGrid)[jCell].getValue();
+                int phi = random.nextInt(3) - 1;
+
+                int v = (int) Math.ceil(Xi + phi * Math.abs(Xi - Xk));
+
+                if (v > 9){
+                    v = (v % 9) + 1;
+                }
+
+                // If third constraint is violated do a swap
+                boolean swap = false;
+                for(int j = 0; j < 9; j++){
+                    if(j != jCell){
+                        if(foodSources.get(i).getGrid(jGrid)[j].getValue() == v){
+                            swap = true;
+
+                            Sudoku candidateSudoku = new Sudoku(foodSources.get(i).getBoard());
+
+                            candidateSudoku.getGrid(jGrid)[jCell].setValue(v);
+                            candidateSudoku.getGrid(jGrid)[j].setValue(Xi);
+
+                            candidateSudoku.calculateFitness();
+
+                            if(candidateSudoku.getFitnessLevel() > foodSources.get(i).getFitnessLevel()){
+                                foodSources.set(i, candidateSudoku);
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if(!swap){
+                    Sudoku candidateSudoku = new Sudoku(foodSources.get(i).getBoard());
+
+                    candidateSudoku.getGrid(jGrid)[jCell].setValue(v);
+    
+                    candidateSudoku.calculateFitness();
+    
+                    if(candidateSudoku.getFitnessLevel() > foodSources.get(i).getFitnessLevel()){
+                        System.out.println("HELLO");
+                        foodSources.set(i, candidateSudoku);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @return Fitness summation of all candidate solutions in population
+     */
+    public float getTotalNector(){
+        float totalNector = 0;
+
+        for(Sudoku s : foodSources){
+            totalNector += s.getFitnessLevel();
+        }
+
+        return totalNector;
     }
 
     public static void main(String[] args) {
@@ -60,6 +145,10 @@ public class ArtificialBeeColony{
         System.out.println(abc.foodSources.get(0).getFitnessLevel());
         System.out.println(abc.foodSources.get(0));
 
-        System.out.println(abc.totalNector);
+        System.out.println(abc.getTotalNector());
+
+        abc.NeighborhoodSearch();
+        
+        System.out.println(abc.getTotalNector());
     }
 }

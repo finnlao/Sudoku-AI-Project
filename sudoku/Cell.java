e6e8f842258479abe572012b1e3b215b20bd6a25
finnlao <finn.lao@hotmail.com>
2018-11-30 12:35:46 -0500

    removed initializer for possible values in Cell

--- sudoku/Cell.java
+++ sudoku/Cell.java
@@ -29,11 +29,12 @@ public class Cell {
     public Cell(Cell c){
         this.value = c.getValue();
         this.fixed = c.isFixed();
-        this.possibleValues = new HashSet<Integer>();
         
-        Iterator<Integer> it = c.getPossibleValues().iterator();
-        while(it.hasNext()){
-            this.possibleValues.add(it.next().intValue());
+        if(c.getPossibleValues() != null){
+            Iterator<Integer> it = c.getPossibleValues().iterator();
+            while(it.hasNext()){
+                this.possibleValues.add(it.next().intValue());
+            }
         }
     }
 
